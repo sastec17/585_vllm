7f1e3466d412b9bf507757947b8a74576309f9dc
Sarah Stec <sastec@umich.edu>
2024-11-15 15:32:22 -0500

    Download chatbot queries. Partial implementation of custom dataset creation script

--- custom_benchmarks/benchmark_latency.py
+++ custom_benchmarks/benchmark_latency.py
@@ -1,28 +1,64 @@
-"""Benchmark the latency of processing a single batch of requests."""
+"""Benchmark the latency of processing a single batch of requests.
+
+Use Oracle w/ output lengths to establish priority
+
+"""
 import argparse
 import dataclasses
 import json
 import time
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import numpy as np
 import torch
 from tqdm import tqdm
+import random
 
 from vllm import LLM, SamplingParams
 from vllm.engine.arg_utils import EngineArgs
 from vllm.inputs import PromptType
 from vllm.utils import FlexibleArgumentParser
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
+# HELPER FUNCTIONS 
+######################################################################
+def _get_data(
+    dataset_path: str,
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase
+):
+    with open(dataset_path, 'r') as file:
+        dataset = json.load(file)
+        dataset = dataset[0:100]
+    # Shuffle dataset
+    random.shuffle(dataset)
+    filtered_dataset: List[Tuple[str, int]] = []
+    for i in range((len(dataset))):
+        if len(filtered_dataset) == num_requests:
+            break
+        # tokenize prompt inputs to filter
+        prompt = dataset[i]['input']
+        prompt_token_ids = tokenizer(prompt).input_ids
+        prompt_len = len(prompt_token_ids)
+        output_len = dataset[i]['output_token_len']
+        if prompt_len < 4 or output_len < 4:
+            # Prune too short sequences.
+            continue
+        if prompt_len > 1024 or prompt_len + output_len > 2048:
+            # Prune too long sequences.
+            continue
+        # priority = output_len
+        filtered_dataset.append((prompt, output_len))
+    return filtered_dataset
+
+
+# MAIN
+######################################################################
 def main(args: argparse.Namespace):
-    print(args)
-
     engine_args = EngineArgs.from_cli_args(args)
     print(engine_args)
 
-    # NOTE(woosuk): If the request cannot be processed in a single batch,
-    # the engine will automatically process the request in multiple batches.
     llm = LLM(**dataclasses.asdict(engine_args))
 
     sampling_params = SamplingParams(
@@ -32,18 +68,16 @@ def main(args: argparse.Namespace):
         ignore_eos=True,
         max_tokens=args.output_len,
     )
-    print(sampling_params)
     
-    # TODO - load small portion of a dataset w/ variance in answers + select random portion from ds
-    # TODO - tokenize inputs + send to dataset
-    sample_prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
-    ]
-    # Re-vamp dummy prompts sent in to use actual dataset of stuff
-
+    filtered_dataset = _get_data()
+    # Separate prompts and priorities
+    prompts = []
+    sampling_params = []
+    priorities = []
+    for prompt, _priority in filtered_dataset:
+        prompts.append(prompt)
+        priorities.append(_priority)
+        # TODO: use diff sampling_params? 
     # Write two versions of data extraction function 
     # Use random CL flag with model  -> based on that extract data we need + initialize LLM as needed 
     def run_to_completion(profile_dir: Optional[str] = None):
@@ -55,24 +89,29 @@ def main(args: argparse.Namespace):
                     ],
                     on_trace_ready=torch.profiler.tensorboard_trace_handler(
                         str(profile_dir))) as p:
-                llm.generate(sample_prompts,
+                llm.generate(prompts,
                              sampling_params=sampling_params,
                              use_tqdm=False)
             print(p.key_averages())
         else:
             start_time = time.perf_counter()
-            if args.scheduling_policy == 'priority':
-                print("attemting priority rr")
-                priorities = [2,3,4,1]
-                llm.generate(sample_prompts,
+            if args.scheduling_policy == 'fcfs':
+                llm.generate(prompts,
+                            sampling_params=sampling_params,
+                            use_tqdm=False)
+        
+            elif args.scheduling_policy == 'priority':
+                print('priority rr')
+                llm.generate(prompts,
                             sampling_params=sampling_params,
                             use_tqdm=False,
                             priority=priorities)
 
-            else:
-                llm.generate(sample_prompts,
+            elif args.scheduling_policy == 'priority_round_robin':
+                llm.generate(prompts,
                             sampling_params=sampling_params,
                             use_tqdm=False)
+            
             end_time = time.perf_counter()
             latency = end_time - start_time
             return latency
@@ -81,7 +120,7 @@ def main(args: argparse.Namespace):
     for _ in tqdm(range(args.num_iters_warmup), desc="Warmup iterations"):
         run_to_completion(profile_dir=None)
 
-    # TODO: ADD IN PROFILE STUFF
+    # TODO: ADD IN PROFILE STUFF FROM OG FILE
     # Benchmark.
     latencies = []
     for _ in tqdm(range(args.num_iters), desc="Profiling iterations"):
@@ -134,7 +173,13 @@ if __name__ == '__main__':
         default=None,
         help=('path to save the pytorch profiler output. Can be visualized '
               'with ui.perfetto.dev or Tensorboard.'))
-
+    parser.add_argument(
+        '--input-json',
+        type=str,
+        default=None,
+        required=True,
+        help='Path to input data file.'
+    )
     parser.add_argument(
         '--output-json',
         type=str,

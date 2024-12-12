8fde06a3490d74f30018aedc17a31c5f4e5449cf
justinjpaul <justpaul@umich.edu>
2024-12-11 19:59:36 -0500

    feat: add graphs for llama run

--- custom_benchmarks/graphs/ttft_boxplots.py
+++ custom_benchmarks/graphs/ttft_boxplots.py
@@ -5,22 +5,23 @@ import seaborn as sns
 
 
 # EDITABLE
-MODEL = "facebook_opt-125m"
+MODEL = "meta-llama_Llama-3.2-1B"
 OUTPUT_LENGTH = 1024
 # STOP EDITING
 
 
-JSON_DIR = f"../data/{MODEL}/{OUTPUT_LENGTH}/"
+JSON_DIR = f"../data/{MODEL}/{OUTPUT_LENGTH}/5/"
 JSONS = {
-    "FCFS": "fcfs_o.json",
     "SJF": "priority_o.json",
-    "Round-robin SJF": "priority_round_robin_o.json",
+    "Round Robin SJF": "priority_round_robin_o.json",
+    "Round Robin": "round_robin_o.json",
+    "FCFS": "fcfs_o.json",
 }
-DATA_STRING = "ttfts"
+DATA_STRING = "e2els"
 
-GRAPH_TITLE = "Scheduling Algorithm TTFTs"
+GRAPH_TITLE = f"Scheduling Algorithm {DATA_STRING.upper()}"
 X_LABEL = "Scheduling Policy"
-Y_LABEL = "TTFT (s)"
+Y_LABEL = f"{DATA_STRING.upper()} (s)"
 # adjust graph fontsizes changing stuff
 
 
@@ -40,7 +41,11 @@ for name, file in JSONS.items():
 
 df = pd.DataFrame(data)
 
-sns.boxplot(df, x=X_LABEL, y=Y_LABEL)
 
+sns.violinplot(df, x=X_LABEL, y=Y_LABEL)
 plt.title(GRAPH_TITLE)
-plt.savefig(f"ttft_boxplots_{MODEL}_{OUTPUT_LENGTH}.png")
+plt.savefig(f"{DATA_STRING}_violinplots_{MODEL}_{OUTPUT_LENGTH}.png")
+
+sns.boxplot(df, x=X_LABEL, y=Y_LABEL)
+plt.title(GRAPH_TITLE)
+plt.savefig(f"{DATA_STRING}_boxplots_{MODEL}_{OUTPUT_LENGTH}.png")
--- custom_benchmarks/graphs/ttft_boxplots_noise.py
+++ custom_benchmarks/graphs/ttft_boxplots_noise.py
@@ -2,20 +2,26 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import os
+from pathlib import Path
+from glob import glob
 
+os.getcwd()
 
 # EDITABLE
-MODEL = "facebook_opt-125m"
+MODEL = "meta-llama_Llama-3.2-1B"
 OUTPUT_LENGTH = 1024
 # STOP EDITING
 
 
+glob("../data/meta-llama_llama-3.2-1B/noise/*")
+
 JSON_DIR = f"../data/{MODEL}/noise/"
 DATA_STRING = "ttfts"
 
-GRAPH_TITLE = "Scheduling Algorithm TTFTs"
+GRAPH_TITLE = f"SJF vs RR-SJF {DATA_STRING.upper()} With Noise"
 X_LABEL = "STDEV"
-Y_LABEL = "TTFT (s)"
+Y_LABEL = f"{DATA_STRING.upper()} (s)"
 
 
 def read_json_data(file_path) -> float:
@@ -26,9 +32,12 @@ def read_json_data(file_path) -> float:
 
 # Read data from JSONs
 data = []
-for stdev in range(25, 151, 25):
-    for policy in ["priority", "priority_round_robin"]:
-        file = JSON_DIR + f"noise_{stdev}_{policy}_o.json"
+for stdev in range(0, 151, 25):
+    for policy, ext in {
+        "SJF": "priority",
+        "RR-SJF": "priority_round_robin_reverse",
+    }.items():
+        file = JSON_DIR + f"noise_{stdev}_{ext}_o.json"
 
         data.extend(
             [
@@ -37,12 +46,15 @@ for stdev in range(25, 151, 25):
             ]
         )
 
-for policy in ["priority", "priority_round_robin"]:
-    file = f"../data/{MODEL}/{OUTPUT_LENGTH}/" + f"{policy}_o.json"
+# for policy, ext in {
+#     "SJF": "priority",
+#     "RR-SJF": "priority_round_robin_reverse",
+# }.items():
+#     file = f"../data/{MODEL}/{OUTPUT_LENGTH}/" + f"{ext}_o.json"
 
-    data.extend(
-        [{X_LABEL: 0, Y_LABEL: ttft, "Policy": policy} for ttft in read_json_data(file)]
-    )
+#     data.extend(
+#         [{X_LABEL: 0, Y_LABEL: ttft, "Policy": policy} for ttft in read_json_data(file)]
+#     )
 
 data
 df = pd.DataFrame(data)
@@ -53,4 +65,4 @@ df.groupby(["STDEV", "Policy"]).describe()
 sns.boxplot(df, x=X_LABEL, y=Y_LABEL, hue="Policy")
 
 plt.title(GRAPH_TITLE)
-plt.savefig(f"noise_ttft_boxplots_{MODEL}_{OUTPUT_LENGTH}.png")
+plt.savefig(f"noise_{DATA_STRING}_boxplots_{MODEL}_{OUTPUT_LENGTH}.png")
